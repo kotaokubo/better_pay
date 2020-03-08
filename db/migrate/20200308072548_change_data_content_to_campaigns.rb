daad5441f35da34ed228272606700832bd44143a
kotaokubo <kutpremium@gmail.com>
2020-03-08 17:41:24 +0900

    change_columnで型をintegerに変更すると失敗する問題を解決

--- db/migrate/20200308072548_change_data_content_to_campaigns.rb
+++ db/migrate/20200308072548_change_data_content_to_campaigns.rb
@@ -1,5 +1,5 @@
 class ChangeDataContentToCampaigns < ActiveRecord::Migration[6.0]
   def change
-    change_column :campaigns, :content, :integer
+    change_column :campaigns, :content, 'integer USING CAST(content AS integer)'
   end
 end
